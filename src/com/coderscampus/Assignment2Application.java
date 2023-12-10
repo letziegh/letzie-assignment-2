cb095df2afa987745385b90355d268b0a82c1f99
letziegh <148257470+letziegh@users.noreply.github.com>
2023-12-09 19:06:15 -0500

    Update 4 for Assignement 2

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-	
-		
+
 		Scanner scanner = new Scanner(System.in);
 
 		String userInput = collectInput(scanner, "Pick a number between 1 and 100 ");
@@ -16,24 +15,24 @@ public class Assignment2Application {
 
 		Random random = new Random();
 		int randomNumber = random.nextInt(101);
-		for (int i= 1; i<5; i++) {
-			
-			Scanner scan = new Scanner(System.in);
-		if (convertedInput == randomNumber) {
-			
-							System.out.println("You win!");
-						} else if (convertedInput < randomNumber) {
-							System.out.println("Please pick a higher number");
-						} else if (convertedInput > randomNumber) {
-							System.out.println("Please pick a lower number");
-			
-						}
-						System.out.println("You lose, the number to guess was " + randomNumber);
-					}
-	}
 
-		
-	
+		for (int i = 1; i < 5; i++) {
+
+			Scanner scan = new Scanner(System.in);
+
+			if (convertedInput == randomNumber) {
+
+				System.out.println("You win!");
+			} else if (convertedInput < randomNumber) {
+				System.out.println("Please pick a higher number");
+			} else if (convertedInput > randomNumber) {
+				System.out.println("Please pick a lower number");
+
+			}
+		}
+		System.out.println("You lose, the number to guess was " + randomNumber);
+
+	}
 
 	private static String collectInput(Scanner scanner, String messageInConsole) {
 		System.out.println(messageInConsole);
@@ -41,7 +40,7 @@ public class Assignment2Application {
 		return userInput;
 
 //		for (int i = 1; i < 5; i++) {
-			// System.out.println(randomNumber);
+		// System.out.println(randomNumber);
 //	System.out.println("You lose, the number to guess was " + randomNumber);
 
 //LOOP IF STATEMENT 5 TIMES THEN END PROGRAM////
