d33213c7a2f3e1630dbdae0ad842c1a835dc1c65
letziegh <148257470+letziegh@users.noreply.github.com>
2023-12-19 14:24:53 -0500

    Update 7 on assignment 2

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -12,70 +12,44 @@ public class Assignment2Application {
 		// String userInput = collectInput(scanner, "Pick a number between 1 and 100 ");
 		// changed to scanner
 
-		
 		// int convertedInput = Integer.parseInt(userInput);
 		Random random = new Random();
 		int randomNumber = random.nextInt(101);
 		System.out.println(randomNumber);
 		System.out.println("Please pick a number between 1 and 100");
-		int convertedInput = scanner.nextInt();
-		
-			
-		
-//		while(convertedInput <1 || convertedInput>100) {
-//		  
-//				
-//				System.out.println("Your guess is not between 1 and 100, please try again");
-//				convertedInput = scanner.nextInt();
-//		
-//		}
-		
+		// int convertedInput = scanner.nextInt();
 
-		for (int i = 0; i < 4; i++) {
-			
-			if (convertedInput == randomNumber) {
-				
-				System.out.println("You win!");
-	
-				
-			} else if (convertedInput < randomNumber) {
-				System.out.println("Please pick a higher number");
-			} else if (convertedInput > randomNumber) {
-				System.out.println("Please pick a lower number");
+		// INSERT WHILE LOOP INSTEAD OF FOR LOOP
+
+		int userGuess = 0;
+
+		while (userGuess < 5) {
+			int convertedInput = scanner.nextInt();
+
+			if (convertedInput < 1 || convertedInput > 100) {
+				System.out.println("Your guess is not between 1 and 100, please try again");
+				continue;
 
 			}
-			convertedInput = scanner.nextInt();
+			userGuess++;
+			// for (int i = 0; i < 4; i++) {
+
+			if (convertedInput == randomNumber) {
+
+				System.out.println("You win!");
+				return;
+
+			} else if (convertedInput < randomNumber) {
+				System.out.println("Please pick a higher number");
+			} else {
+
+				System.out.println("Please pick a lower number");
+			}
+
 		}
-		
+
 		System.out.println("You lose, the number to guess was: " + randomNumber);
-		
+
 	}
 }
-
-	
-
-//	private static String collectInput(Scanner scanner, String messageInConsole) {
-//		System.out.println(messageInConsole);
-//		String userInput = scanner.nextLine();
-	//	return userInput;
-
-//		for (int i = 1; i < 5; i++) {
-		// System.out.println(randomNumber);
-//	System.out.println("You lose, the number to guess was " + randomNumber);
-
-//LOOP IF STATEMENT 5 TIMES THEN END PROGRAM////
-
-//			if (convertedInput == randomNumber) {
-//
-//				System.out.println("You win!");
-//			} else if (convertedInput < randomNumber) {
-//				System.out.println("Please pick a higher number");
-//			} else if (convertedInput > randomNumber) {
-//				System.out.println("Please pick a lower number");
-//
-//			}
-//			System.out.println("You lose, the number to guess was " + randomNumber);
-//		}
-	
-
-
+//convertedInput = scanner.nextInt();
